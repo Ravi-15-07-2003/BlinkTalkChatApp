197a9688201e192c3bc0e0759098b3bc85b9e8b9
Ravi Singh bais <ravisinghbais5@gmail.com>
2025-06-28 17:24:49 +0530

    error firxing again in backend

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,8 @@ let app = express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173"
+        origin:"http://localhost:5173",
+        "https://blink-talk-chat-app.vercel.app",
     }
 })
  const userSocketMap ={}
